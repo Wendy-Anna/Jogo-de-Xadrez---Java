c16c4ffabfa14dd791c0cd2543fa2e834b4c9efd
Wendy-Anna <wendyufpb@gmail.com>
2020-01-28 15:04:20 -0300

    Primeira classe

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word!");
+		
+		Posicao pos = new Posicao (3,5);
+		System.out.println(pos);
 	}
 
 }
