32fffcd464e429a14f571e4d2e9bfbb45c9ac7f1
Wendy-Anna <wendyufpb@gmail.com>
2020-01-29 07:48:09 -0300

    Classe UI

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -7,6 +7,8 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		PartidaXadrez partidaXadrez = new PartidaXadrez();
+		UI.printTabuleiro(partidaXadrez.getPecas());
+
 	}
 
 }
